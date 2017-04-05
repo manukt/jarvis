87db2522cbb1a5ceb92a7954d8ac628cfe27908e
manu kt <manu.kt@gdn-commerce.com>
2017-04-06 00:08:54 +0530

    spring boot servlet initializer

--- src/main/java/com/jarvis/apps/SpringbootJarvisApplication.java
+++ src/main/java/com/jarvis/apps/SpringbootJarvisApplication.java
@@ -2,9 +2,16 @@ package com.jarvis.apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootJarvisApplication {
+public class SpringbootJarvisApplication extends SpringBootServletInitializer{
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootJarvisApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJarvisApplication.class, args);
